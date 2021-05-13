7c48a99d7e6dee7fe7c62b138e2a6058d1095032
sumedhBodas <79521023+sumedhBodas@users.noreply.github.com>
2021-05-13 20:58:43 +0530

    Subscribe Page
    User will be directed here once he hits "subscribe" button.

---
+++ Subscribe.php
@@ -0,0 +1,20 @@
+<?php
+session_start();
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div style="text-align: center;">
+    <h1> Thank you for subscribing </h1>
+    <h2> You have successfully subscribed to XKCD comic. Please check your mail to enjoy the comic </h2>
+</div>
+<body>
+</html>
