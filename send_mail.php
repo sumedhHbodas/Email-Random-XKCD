e32bbb2eb8c647eb204064e75b8c5b85d935450c
sumedhBodas <79521023+sumedhBodas@users.noreply.github.com>
2021-05-13 08:43:32 +0530

    Update send_mail.php

--- send_mail.php
+++ send_mail.php
@@ -2,15 +2,23 @@
 session_start();
 if(!$_SESSION['name'])
 {
-    header('location:Login_Page.php');
+    header('location: Login_page.php');
 }
-
 echo $_SESSION['name'];
-echo "<h1> mail will be send from here </h1>"
-
-                
-           
-            
-
+echo "<h1> mail will be send from here </h1>";
 ?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <span style="padding: 10px; border: 1px solid black;">
+    <a href= "Logout_Page.php" > Logout
+</span>
+</body>
+</html>
