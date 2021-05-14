686e9d897a87cd9041b1d71ebabd602230f2a1a6
sumedhBodas <79521023+sumedhBodas@users.noreply.github.com>
2021-05-14 22:56:07 +0530

    Integrating sendgrid API
    Integrating sendgrid API to fix the email sending failure bug occurring in Heroku

---
+++ mailSender.php
@@ -0,0 +1,26 @@
+<?php
+ require 'vendor/autoload.php';
+
+    class sendEmail {
+        public static function sendMail($to, $subject, $content)
+        {
+            $key = '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';
+            $mail = new sendgrid\Mail\Mail();
+            $mail->setFrom('shbodas28@gmail.com', "Sumedh Bodas");
+            $mail->setSubject($subject);
+            $mail->addTo($to);
+            $mail->addContent('text/plain', $content);
+
+            $sendgrid = new \sendgrid($key);
+            try {
+                $response = $sendgrid->send($mail);
+                return $response;
+            } catch (exception $e) {
+                echo "Mail exception caught". $e->getMessage(). "\n";
+                return false;
+            }
+
+        }
+    }
+
+?>
