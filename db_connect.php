a29fe143cced3a8841f39b5798be05704c673315
sumedhBodas <79521023+sumedhBodas@users.noreply.github.com>
2021-05-14 09:20:43 +0530

    Remote Database connection
    Local database transferred to remote MYSQL database.

--- db_connect.php
+++ db_connect.php
@@ -1,9 +1,17 @@
 <?php
 
-$server = "localhost:3307";
+// Local Databasse
+/* $server = "localhost:3307";
 $username = "root";
 $password = "";
-$database = "email";
+$database = "email"; */
+
+//remote Database
+$server = "remotemysql.com";
+$username = "8DXrjewoaV";
+$password = "kxRpTMROM1";
+$database = "8DXrjewoaV";
+
 
 $db_connect = mysqli_connect($server, $username, $password, $database);
 
